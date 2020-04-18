b31eac3a108ce93748155495d92412bb27d15ed9
omrobbie <putu.roby@yahoo.com>
2020-04-18 13:43:41 +0800

    prepare table view and cell

---
+++ AutoSizingTableCell/TableCell.swift
@@ -0,0 +1,12 @@
+//
+//  TableCell.swift
+//  AutoSizingTableCell
+//
+//  Created by omrobbie on 18/04/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+}
